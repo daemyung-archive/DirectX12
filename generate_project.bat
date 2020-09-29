b2e493e476a898a9602524ebd311e8d20886ce00
daemyung jang <dm86.jang@gmail.com>
2020-09-29 16:13:59 +0900

    Remove a bash script to help generate project

--- generate_project.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-FOR /F "tokens=*" %%g IN ('where.exe vcpkg') do (SET VCPKG_PATH=%%g)
-SET CMAKE_TOOLCHAIN_FILE=%VCPKG_PATH:vcpkg.exe=scripts\buildsystems\vcpkg.cmake%
-
-SET BUILD_DIR=build
-MD %BUILD_DIR%
-cmake . -B %BUILD_DIR% -DCMAKE_TOOLCHAIN_FILE=%CMAKE_TOOLCHAIN_FILE%
