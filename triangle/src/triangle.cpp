fc24684b9a2a92b4e17620a8fe914dea26bc2915
daemyung jang <dm86.jang@gmail.com>
2020-10-22 13:51:58 +0900

    Fix a bug

--- triangle/src/triangle.cpp
+++ triangle/src/triangle.cpp
@@ -192,8 +192,8 @@ private:
     void InitPipelines() {
         // Define an input layout.
         std::vector<D3D12_INPUT_ELEMENT_DESC> input_layout = {
-                {"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT,    0, 0,  D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0},
-                {"COLOR",    0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0}};
+                {"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0,  D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0},
+                {"COLOR",    0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0}};
 
         // Define a root parameter.
         CD3DX12_ROOT_PARAMETER root_parameter;
